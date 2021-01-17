b83ea6952f503f80d880d25f4bc2d1b9ca9acf79
Max Gerwens (inf1272) <max.gerwens@smail.th-koeln.de>
2021-01-17 17:23:59 +0100

    Implement WorkManager OneTimeRequest

--- app/src/main/java/de/backgroundoperrations/mocoworkshop/ui/work/WorkFragment.kt
+++ app/src/main/java/de/backgroundoperrations/mocoworkshop/ui/work/WorkFragment.kt
@@ -1,12 +1,18 @@
 package de.backgroundoperrations.mocoworkshop.ui.work
 
+
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
 import de.backgroundoperrations.mocoworkshop.R
+
 
 class WorkFragment : Fragment() {
 
@@ -18,8 +24,13 @@ class WorkFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         workViewModel =
-                ViewModelProvider(this).get(WorkViewModel::class.java)
+            ViewModelProvider(this).get(WorkViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_work, container, false)
+        val request= OneTimeWorkRequestBuilder<WorkOneTimeRequest>().build()
+        val btnonetime =root.findViewById<Button>(R.id.btnOnetimerequest)
+        btnonetime.setOnClickListener {
+            WorkManager.getInstance().enqueue(request)
+        }
 
         return root
     }

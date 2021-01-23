03c92218e8b755fa9514b61406e9d551ccd02b2c
Max Gerwens (inf1272) <max.gerwens@smail.th-koeln.de>
2021-01-23 12:13:56 +0100

    Update MyService.kt

--- app/src/main/java/de/backgroundoperrations/mocoworkshop/ui/myservices/MyService.kt
+++ app/src/main/java/de/backgroundoperrations/mocoworkshop/ui/myservices/MyService.kt
@@ -62,6 +62,7 @@ class MyService : Service() {
                         shownote(mServerMessage)
                         Log.i("connectserver", mServerMessage)
                     }
+
             }
 
         }

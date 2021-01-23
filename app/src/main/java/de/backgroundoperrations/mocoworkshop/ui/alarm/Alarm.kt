137ff763cfa8fd04ccdd47f92ff6e8c6a735b874
aomari99 <52887341+aomari99@users.noreply.github.com>
2021-01-23 23:19:25 +0100

    Add my Tasks and ui fixes
    Nav view updates and added alarm and thread

---
+++ app/src/main/java/de/backgroundoperrations/mocoworkshop/ui/alarm/Alarm.kt
@@ -0,0 +1,17 @@
+package de.backgroundoperrations.mocoworkshop.ui.alarm
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.media.MediaPlayer
+import android.widget.Toast
+import de.backgroundoperrations.mocoworkshop.R
+
+class Alarm :BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        val mp = MediaPlayer.create(context, R.raw.beep)
+        mp.start()
+        Toast.makeText(context,"Alarm",Toast.LENGTH_LONG).show()
+    }
+}
--- app/src/main/java/de/backgroundoperrations/mocoworkshop/ui/threads/ThreadFragment.kt
+++ app/src/main/java/de/backgroundoperrations/mocoworkshop/ui/threads/ThreadFragment.kt
@@ -1,14 +1,25 @@
 package de.backgroundoperrations.mocoworkshop.ui.threads
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.Button
+import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
 import de.backgroundoperrations.mocoworkshop.R
 import de.backgroundoperrations.mocoworkshop.ui.work.WorkViewModel
-
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.net.HttpURLConnection
+import java.net.URL
+import android.widget.*
 
 class ThreadFragment : Fragment() {
 
@@ -22,7 +33,54 @@ class ThreadFragment : Fragment() {
         threadViewModel =
             ViewModelProvider(this).get(ThreadViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_thread, container, false)
+        var connect  = root.findViewById<Button>(R.id.connect);
+        var locations : MutableList<String> = mutableListOf()
+        GlobalScope.launch{
 
-        return root
+            var reque = URL("https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_Landkreisdaten/FeatureServer/0/query?where=1%3D1&outFields=GEN&returnGeometry=false&outSR=4326&f=json")
+            Log.d("json", reque.toString())
+            val conn: HttpURLConnection = reque.openConnection() as HttpURLConnection
+            conn.setConnectTimeout(1000) // timing out in a minute
+            val json = conn.inputStream.bufferedReader().use(BufferedReader::readText)
+
+            Log.d("json", json)
+            val field = JSONObject(json).getJSONArray("features")
+            for (i in 0..(field.length()-1)) {
+                val attr = field.getJSONObject(i)
+                val ele = attr.getJSONObject("attributes")
+                val cases = ele.getString("GEN")
+                locations.add(cases)
+            }
+        }
+        val ad : ArrayAdapter<String> = ArrayAdapter<String>(root.context,R.layout.support_simple_spinner_dropdown_item,locations)
+
+        var iptxt = root.findViewById<AutoCompleteTextView>(R.id.iptxt)
+        iptxt.setAdapter(ad)
+        var hello = root.findViewById<TextView>(R.id.info)
+        connect.setOnClickListener() {
+            var i = 0;
+            GlobalScope.launch {
+                while (true) {
+                    i++;
+                }
+            }.start()
+
+            Thread {
+
+                var reque = URL("https://services7.arcgis.com/mOBPykOjAyBO2ZKk/arcgis/rest/services/RKI_Landkreisdaten/FeatureServer/0/query?where=GEN='${iptxt.text}'&outFields=cases7_per_100k_txt&returnGeometry=false&outSR=4326&f=json")
+                Log.d("json", reque.toString())
+                val conn: HttpURLConnection = reque.openConnection() as HttpURLConnection
+                conn.setConnectTimeout(1000) // timing out in a minute
+                val json = conn.inputStream.bufferedReader().use(BufferedReader::readText)
+                val field = JSONObject(json).getJSONArray("features")
+                val attr = field.getJSONObject(0)
+                val ele = attr.getJSONObject("attributes")
+                val cases = ele.getString("cases7_per_100k_txt")
+
+                hello.text = "$cases Fälle pro 100.000 Einwohner in den Letzen 7 Tagen"
+
+            }.start()
+
     }
-}
+        return root
+}}
